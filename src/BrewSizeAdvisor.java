e761e66b295abd10d308bf50e316493c7cf35018
praveenm0808 <praveenmurugavel08@gmail.com>
2026-01-19 15:53:09 +0530

    Update BrewSizeAdvisor.java

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -10,7 +10,7 @@ public class BrewSizeAdvisor {
         if(volume>350){
             System.out.println("Large");
         }
-        else if(volume >250 && volume <=350){
+        else if(volume >200 && volume <=350){
             System.out.println("Medium");
         }
         else{
